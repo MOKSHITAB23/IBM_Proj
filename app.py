11431bdacef8164d9406e1bd2b4887b303e65865
MOKSHITAB23 <mokshitabajpai@gmail.com>
2025-11-02 20:13:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
